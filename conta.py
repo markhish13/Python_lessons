99ff51c928d9420fb95f5887ad0b3ff98500a90a
markhish13 <44106013+markhish13@users.noreply.github.com>
2020-03-08 19:10:42 -0300

    Add files via upload

--- conta.py
+++ conta.py
@@ -23,12 +23,20 @@ class Conta:
             print("Transação não efetuada - Saldo Indisponível")
             sdo_dispo += valor
 
-
-    def alterar_limite(self, valor):
-        self.__limite += valor
-
     def tranferir(self, valor, destino):
         self.sacar(valor)
         destino.depositar(valor)
 
+    def get_saldo(self):
+        return self.__saldo
 
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
